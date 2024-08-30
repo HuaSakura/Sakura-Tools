2915b3748e65ad71b1e7f2544fdf1419a92a37f4
SakuraHan <173314746@qq.com>
2024-08-30 23:39:27 +0800

    fix: 调整样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="index">
-    <div class="index-left">
-      <div class="index-left-content">
-        <img src="../assets/favicon.ico" alt="" @click="openOfficialWebsite">
-      </div>
-    </div>
-    <div class="index-right">
-
-    </div>
+    这是首页
   </div>
 </template>
 
@@ -18,42 +11,16 @@ export default defineComponent({
   name: "index",
   setup() {
 
-    function openOfficialWebsite() {
-      window.ipcRenderer.send('open-official-website')
-    }
-
     return {
-      openOfficialWebsite
+
     }
   }
 })
 </script>
 
 <style scoped lang="less">
-.index{
-  width: 100vw;
-  height: 100vh;
-  background-color: #343434;
-
-  .index-left {
-    width: 78px;
-    height: 100vh;
-    background-color: #191919;
-
-    .index-left-content {
-      padding-top: 36px;
-      width: 100%;
-      height: 78px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-
-      img {
-        width: 48px;
-        height: 48px;
-      }
-    }
-  }
+.index {
+  width: 100%;
+  height: 100%;
 }
-
 </style>
